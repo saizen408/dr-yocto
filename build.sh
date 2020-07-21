31349d2e0cc7d5559e7e68a126aac6e24f1b7d00
Eric Owusu <eowusu+bitbucket@accretechsbs.com>
2020-07-21 14:50:00 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # argument 1: Ubuntu version (e.g. 18.04) used as container tag
 # argument 2: path to Dockerfile
 
-docker build --build-arg "host_uid=$(id -u)" --build-arg "host_gid=$(id -g)" --tag "dr-yocto:$1" $2
+docker build --build-arg "host_uid=99" --build-arg "host_gid=99" --tag "dr-yocto:$1" $2
 
